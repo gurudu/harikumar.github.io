ecbf8b731adb3163eca2b87d8831ede745224759
Hari kumar Gurudu <gharikumar2@gmail.com>
2017-08-17 14:21:06 +0200

    changed github project url

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,7 +3,7 @@ const projectsData = [
       title: 'Portfolio',
       date: '2017',
       github_url: 'https://github.com/gurudu/harikumar.github.io',
-      project_url: '',
+      project_url: 'https://hari-gurudu-portfolio.herokuapp.com/',
       background: 'This is my brand new react application using create-react-app and which presents the details of my projects and skills ',
       role: 'Front-end Developer',
       tech_stack: ['React.js','ES6','SCSS','heroku']
@@ -20,6 +20,8 @@ const projectsData = [
 {
   title: 'Math Equation editor(SciFlow GmBH)',
   date: '2017',
+  github_url: '',
+  project_url: '',
   background: 'Math Equation editor allows the user to enter Scientific equations into the rich text editor ',
   role: 'Front-end Developer and TDD',
   tech_stack: ['HTML5','CSS3','TypeScript','ES6','Angular 4','AngularMaterial','SCRUM','e2eTesting(Jasime and Protractor)']
