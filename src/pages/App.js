347a0c7f029fbed3d9f1e852dcc1c82942ca7f0c
Hari kumar Gurudu <gharikumar2@gmail.com>
2017-09-28 19:20:37 +0200

    Added CV pdf and about

--- src/pages/App.js
+++ src/pages/App.js
@@ -17,7 +17,7 @@ class App extends Component {
            <br />
            <br />
            <h2>Hi, I am Hari Kumar.</h2>
-          <h3>I am an enthusiastic and passionate web developer. I love developing websites, learning  <br/> new technologies and harnessing them to create new and exciting things.</h3> <br/>
+          <h3>I am an enthusiastic and passionate web developer. I love developing websites, learning new technologies and harnessing them to create new and exciting things.</h3> <br/>
           <h5>Please click on <strong>"About"</strong> for more details.</h5>
            </div>
            <div className="col-md-4">
